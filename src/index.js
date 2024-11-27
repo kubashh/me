06857370453bf9287ec49eca560a702f651e6f6b
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-27 12:42:33 +0000

    0.0.1d

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import { App } from "./App"
 
-const root = createRoot(document.getElementById(`root`))
-root.render(
+createRoot(document.getElementById(`root`)).render(
   <StrictMode>
     <App />
   </StrictMode>
