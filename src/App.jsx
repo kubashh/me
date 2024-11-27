17df11173766f1798423e8bb1049dd6cc6d85533
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-27 12:34:42 +0000

    0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>
+        Me
+      </h1>
+      <div>
+        <h2>Programming languages:</h2>
+        <div>JavaScript (node, react)</div>
+        <div>HTML</div>
+        <div>CSS</div>
+        <div>C (bad)</div>
+        <div>C++ (bad)</div>
+        <div>Python (bad)</div>
+        <div>Zig (learning...)</div>
+      </div>
+    </div>
+  )
+}
---
+++ start.sh
@@ -0,0 +1,7 @@
+git add .
+git commit -m $1
+git push
+
+clear
+
+npm run deploy -- -m "u"
