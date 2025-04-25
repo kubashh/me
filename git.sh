b54892bc357874e3fd1afc59be3459523cc6411d
Jakub <jakubhanula0@gmail.com>
2025-04-25 13:52:00 +0200

    0.0.0.16

--- git.sh
+++ git.sh
@@ -1,59 +1,28 @@
-#!/bin/sh
-
 # Oldv
 oldv="0.0.0.0"
 
 # Read old version
-if [ -f ".version" ]; then
-  oldv=$(head -n 1 .version)
-fi
+if [ -f ".version" ]; then oldv=$(head -n 1 .version); fi
 
 # Set vars
 i=0
-for element in $(echo $oldv | tr "." "\n")
-do
-  if [ $i = 0 ]; then
-    v0=$element
-  fi
-
-  if [ $i = 1 ]; then
-    v1=$element
-  fi
-
-  if [ $i = 2 ]; then
-    v2=$element
-  fi
-
-  if [ $i = 3 ]; then
-    v3=$element
-  fi
+for element in $(echo $oldv | tr "." "\n"); do
+  case $i in
+    0) v0=$element;;
+    1) v1=$element;;
+    2) v2=$element;;
+    3) v3=$element;;
+  esac
 
   i=$((i+1))
 done
 
 # Increase version
 case $1 in
-  0)
-    v0=$((v0+1))
-    v1=0
-    v2=0
-    v3=0
-    ;;
-
-  1)
-    v1=$((v1+1))
-    v2=0
-    v3=0
-    ;;
-
-  2)
-    v2=$((v2+1))
-    v3=0
-    ;;
-
-  *)
-    v3=$((v3+1))
-    ;;
+  0) v0=$((v0+1)) && v1=0 && v2=0 && v3=0;;
+  1) v1=$((v1+1)) && v2=0 && v3=0;;
+  2) v2=$((v2+1)) && v3=0;;
+  *) v3=$((v3+1));;
 esac
 
 # Set new version
